436d510ad36ed618cbd6f958a0483f2c008c3a59
99002437 <71481160+99002437@users.noreply.github.com>
2020-11-11 12:08:08 +0530

    Update client.py

--- client.py
+++ client.py
@@ -90,3 +90,107 @@ def serverListen(serverSocket):
 			break
 		else:
 			print(msg)
+
+def userInput(serverSocket):
+	while state["alive"]:
+		state["sendMessageLock"].acquire()
+		state["userInput"] = input()
+		state["sendMessageLock"].release()
+		with state["inputCondition"]:
+			state["inputCondition"].notify()
+		if state["userInput"] == "/1":
+			serverSocket.send(b"/viewRequests")
+		elif state["userInput"] == "/2":
+			serverSocket.send(b"/approveRequest")
+		elif state["userInput"] == "/3":
+			serverSocket.send(b"/disconnect")
+			break
+		elif state["userInput"] == "/4":
+			serverSocket.send(b"/allMembers")
+		elif state["userInput"] == "/5":
+			serverSocket.send(b"/onlineMembers")
+		elif state["userInput"] == "/6":
+			serverSocket.send(b"/changeAdmin")
+		elif state["userInput"] == "/7":
+			serverSocket.send(b"/whoAdmin")
+		elif state["userInput"] == "/8":
+			serverSocket.send(b"/kickMember")
+
+def waitServerListen(serverSocket):
+	while not state["alive"]:
+		msg = serverSocket.recv(1024).decode("utf-8")
+		if msg == "/accepted":
+			state["alive"] = True
+			print("Your join request has been approved. Press any key to begin chatting.")
+			break
+		elif msg == "/waitDisconnect":
+			state["joinDisconnect"] = True
+			break
+
+def waitUserInput(serverSocket):
+	while not state["alive"]:
+		state["userInput"] = input()
+		if state["userInput"] == "/1" and not state["alive"]:
+			serverSocket.send(b"/waitDisconnect")
+			break
+
+def main():
+	if len(sys.argv) < 3:
+		print("USAGE: python client.py <IP> <Port>")
+		print("EXAMPLE: python client.py localhost 8000")
+		return
+	serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	serverSocket.connect((sys.argv[1], int(sys.argv[2])))
+	state["inputCondition"] = threading.Condition()
+	state["sendMessageLock"] = threading.Lock()
+	state["username"] = input("Welcome to PyconChat! Please enter your username: ")
+	state["groupname"] = input("Please enter the name of the group: ")
+	state["alive"] = False
+	state["joinDisconnect"] = False
+	state["inputMessage"] = True
+	serverSocket.send(bytes(state["username"],"utf-8"))
+	serverSocket.recv(1024)
+	serverSocket.send(bytes(state["groupname"],"utf-8"))
+	response = serverSocket.recv(1024).decode("utf-8")
+	if response == "/adminReady":
+		print("You have created the group",state["groupname"],"and are now an admin.")
+		state["alive"] = True
+	elif response == "/ready":
+		print("You have joined the group",state["groupname"])
+		state["alive"] = True
+	elif response == "/wait":
+		print("Your request to join the group is pending admin approval.")
+		print("Available Commands:\n/1 -> Disconnect\n")
+	waitUserInputThread = threading.Thread(target=waitUserInput,args=(serverSocket,))
+	waitServerListenThread = threading.Thread(target=waitServerListen,args=(serverSocket,))
+	userInputThread = threading.Thread(target=userInput,args=(serverSocket,))
+	serverListenThread = threading.Thread(target=serverListen,args=(serverSocket,))
+	waitUserInputThread.start()
+	waitServerListenThread.start()
+	while True:
+		if state["alive"] or state["joinDisconnect"]:
+			break
+	if state["alive"]:
+		print("Available Commands:\n/1 -> View Join Requests (Admins)\n/2 -> Approve Join Requests (Admin)\n/3 -> Disconnect\n/4 -> View All Members\n/5 -> View Online Group Members\n/6 -> Transfer Adminship\n/7 -> Check Group Admin\n/8 -> Kick Member\n")
+		waitUserInputThread.join()
+		waitServerListenThread.join()
+		userInputThread.start()
+		serverListenThread.start()
+	while True:
+		if state["joinDisconnect"]:
+			serverSocket.shutdown(socket.SHUT_RDWR)
+			serverSocket.close()
+			waitUserInputThread.join()
+			waitServerListenThread.join()
+			print("Disconnected from PyconChat.")
+			break
+		elif not state["alive"]:
+			serverSocket.shutdown(socket.SHUT_RDWR)
+			serverSocket.close()
+			userInputThread.join()
+			serverListenThread.join()
+			print("Disconnected from PyconChat.")
+			break
+
+if __name__ == "__main__":
+	main()
